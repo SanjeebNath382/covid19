7a7d8e461308cbdc1bc26a60e73031b37455a57b
Sanjeeb Nath <sanjeebnath382@gmail.com>
2020-04-08 13:52:28 +0530

    Updates

---
+++ precache-manifest.6c3a0bdcd9095e14197c61cde995cf0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e9334616fed152042f21eda19285b7",
+    "url": "/SanjeebNath382/covid19/index.html"
+  },
+  {
+    "revision": "55dfd3977fa959a45a88",
+    "url": "/SanjeebNath382/covid19/static/css/main.78433224.chunk.css"
+  },
+  {
+    "revision": "6feae750a058b6be575e",
+    "url": "/SanjeebNath382/covid19/static/js/2.91f66a16.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/SanjeebNath382/covid19/static/js/2.91f66a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55dfd3977fa959a45a88",
+    "url": "/SanjeebNath382/covid19/static/js/main.a2fcf42e.chunk.js"
+  },
+  {
+    "revision": "8c037c626d32dadb2a0a",
+    "url": "/SanjeebNath382/covid19/static/js/runtime-main.3ecc4761.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/SanjeebNath382/covid19/static/media/image.d7265326.png"
+  }
+]);

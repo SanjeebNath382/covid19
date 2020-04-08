60b9f9cc47ba65d9fc8f06bbbcd9084c30d988d4
Sanjeeb Nath <sanjeebnath382@gmail.com>
2020-04-08 12:57:00 +0530

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Cards from "./components/Cards/Cards"
+import Chart from "./components/Chart/Chart"
+import CountryPicker from './components/CountryPicker/CountryPicker'
+import {fetchData} from './api'
+import Spinner from './spinner/Spinner'
+import styles from './App.module.css'
+import img from './static/image.png'
+export default class App extends Component {
+  state={
+    data:{},
+    country:""
+  }
+  async componentDidMount() {
+   const res= await fetchData();
+    this.setState({data:res});
+   
+  }
+  handleCountryChange=async (country)=>{
+     const fetchedData= await fetchData(country);
+     this.setState({data:fetchedData,country:country})
+  }
+  //console.log()
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+  
+    return (
+      <>
+      <div className={styles.container}>
+      <img src={img} alt="covid19" className={styles.image}></img>
+      {console.log(Object.keys(this.state.data).length === 0 && this.state.data.constructor === Object)}
+      {Object.keys(this.state.data).length === 0 && this.state.data.constructor === Object?<Spinner />:<Cards data={this.state.data}/>}
+      <CountryPicker handleCountryChange={this.handleCountryChange}/>
+      <Chart data={this.state.data} country={this.state.country}/>
+        
+      </div>
+      
+      </>
+    )
+  }
 }
-
-export default App;
